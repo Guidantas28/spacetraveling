f67db7905cfddd2af81c556b275841323961ef9b
Guilherme Dantas <guilhermedantas788@gmail.com>
2022-03-21 14:15:32 -0300

    finished the project

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import style from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={style.headerContainer}>
+      <div className={style.headerContainer}>
+        <Link href="/">
+          <a>
+            <img src="/logo.png" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
